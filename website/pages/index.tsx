cc2ee12da613c1839dca2efd1f771802efdf0f31
Anna-Lea <anna-lea.jakob@08eins.com>
2022-12-01 15:51:19 +0100

    i changed basically nothing haha

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -1,6 +1,6 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
@@ -11,13 +11,9 @@ const Home: NextPage = () => {
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Swiss Photography
-        </h1>
+        <h1 className="text-6xl font-bold">Swiss Photography</h1>
 
-        <p className="mt-3 text-2xl mt-5">
-          Wir fotografieren alles!
-        </p>
+        <p className="text-2xl mt-5">Wir fotografieren alles!</p>
 
         <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
           <a
@@ -51,30 +47,20 @@ const Home: NextPage = () => {
           </a>
 
           <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="#"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
           >
             <h3 className="text-2xl font-bold">Kontakt &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Treten Sie in Kontakt mit uns.
-            </p>
+            <p className="mt-4 text-xl">Treten Sie in Kontakt mit uns.</p>
           </a>
         </div>
       </main>
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
+      <footer className="flex h-24 w-full items-center justify-center border-t bg-neutral-100 bottom-0">
+        <p>Dies ist der Footer.</p>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
