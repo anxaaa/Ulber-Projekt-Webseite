10023cdec07c1215fb8cba22edb9ee3eeaaed80b
Anna-Lea <anna-lea.jakob@08eins.com>
2022-12-09 14:40:37 +0100

    made footer responsive

--- website/components/footer.tsx
+++ website/components/footer.tsx
@@ -4,19 +4,21 @@ type FooterProps = {};
 
 export default function Footer({}: PropsWithChildren<FooterProps>) {
   return (
-    <div className="w-full">
+    <div className="w-full bg-neutral-100">
       {/* Footer */}
-      <footer className="h-24 w-full items-center justify-center border-t bg-neutral-100 bottom-0 text-center">
+      <footer className="w-full items-center justify-center border-t bottom-0 text-center">
         <h1 className="mt-4 text-2xl font-bold">Links</h1>
-        <div className="my-3 flex justify-center space-x-4">
+        <div className="my-3 md:flex justify-center md:space-x-4">
           <a className="underline" href="/AGB">
             Allgemeine Geschäftsbedingungen
           </a>
-          <p>|</p>
+          <br />
+          <p className="hidden md:block">|</p>
           <a className="underline" href="/Impressum">
             Impressum
           </a>
-          <p>|</p>
+          <br />
+          <p className="hidden md:block">|</p>
           <a className="underline" href="/Kontakt">
             Kontakt
           </a>
