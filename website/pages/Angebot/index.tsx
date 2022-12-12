2a3ecd0b3b7810a385d3cd3f7c5bf381c4005f49
Anna-Lea <anna-lea.jakob@08eins.com>
2022-12-12 10:42:55 +0100

    added content to Angebot page

--- website/pages/Angebot/index.tsx
+++ website/pages/Angebot/index.tsx
@@ -14,14 +14,110 @@ const Angebot: NextPage = () => {
       <Navigation />
 
       <main className="flex w-full flex-1 flex-col items-center px-20 text-center h-full">
+        {/* first title section */}
         <h1 className="md:text-6xl text-4xl font-bold mt-10">
           Unsere Dienstleistungen
         </h1>
         <p className="mt-4 text-xl">
           Erhalten Sie einen Überblick über unser Angebot.
         </p>
-      </main>
 
+        {/* main links section */}
+        <h3 className="mt-16 text-2xl font-bold mb-4">
+          Unsere beliebtesten Dienstleistungen:
+        </h3>
+        <div className="flex max-w-xs flex-wrap items-center justify-around md:max-w-4xl mb-16">
+          <a
+            href="/Portfolio"
+            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-red-700 focus:text-red-700"
+          >
+            <h3 className="text-2xl font-bold">&rarr; Hochzeitsfotos</h3>
+          </a>
+          <a
+            href="/Portfolio"
+            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-red-700 focus:text-red-700"
+          >
+            <h3 className="text-2xl font-bold">&rarr; Familienfotos</h3>
+          </a>
+          <a
+            href="/Portfolio"
+            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-red-700 focus:text-red-700"
+          >
+            <h3 className="text-2xl font-bold">&rarr; Kindergeburtstag</h3>
+          </a>
+          <a
+            href="/Portfolio"
+            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-red-700 focus:text-red-700"
+          >
+            <h3 className="text-2xl font-bold">
+              &rarr; Werbung / Firmenportraits
+            </h3>
+          </a>
+          <a
+            href="/Portfolio"
+            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-red-700 focus:text-red-700"
+          >
+            <h3 className="text-2xl font-bold">&rarr; Architektur</h3>
+          </a>
+          <a
+            href="/Portfolio"
+            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-red-700 focus:text-red-700"
+          >
+            <h3 className="text-2xl font-bold">&rarr; Weiteres</h3>
+          </a>
+        </div>
+
+        {/* short info section */}
+        <div className="mb-20">
+          <h3 className="text-2xl font-bold mb-4">
+            Ihr Wunsch ist nicht dabei?
+          </h3>
+          <p>
+            Keine Sorge! Wir gehen auf Wünsche aller Art ein. Kontaktieren Sie
+            uns gerne mit Ihrem persönlichem Wunsch.
+          </p>
+          <p className="font-bold my-6 text-lg">
+            Kontaktieren Sie uns jetzt persönlich oder via Formular und
+            vereinbaren Sie ein Termin!
+          </p>
+          <a
+            href="/Kontakt"
+            className="font-bold text-xl mt-6 mb-20 border rounded-xl p-2 px-4 bg-neutral-100 hover:bg-neutral-200 focus:bg-red-700"
+          >
+            &rarr; Kontakt
+          </a>
+        </div>
+
+        {/* second title section */}
+        <h1 className="md:text-6xl text-4xl font-bold mt-10">Preise</h1>
+        <p className="mt-4 text-xl">Unsere Preisliste:</p>
+
+        {/* price section */}
+        <div className="my-20 max-w-xs md:max-w-2xl w-full">
+          <table className="border border-collapse text-left w-full">
+            <tr className="bg-neutral-100 text-lg">
+              <th className="border w-1/2 pl-2">Zeit</th>
+              <th className="border pl-2">Preis</th>
+            </tr>
+            <tr>
+              <td className="border pl-2">1 Stunde</td>
+              <td className="border pl-2">100 CHF</td>
+            </tr>
+            <tr>
+              <td className="border pl-2">2 Stunden</td>
+              <td className="border pl-2">180 CHF</td>
+            </tr>
+            <tr>
+              <td className="border pl-2">Jede weitere Stunde</td>
+              <td className="border pl-2">+ 60 CHF</td>
+            </tr>
+            <tr>
+              <td className="border pl-2">Ganzer Tag</td>
+              <td className="border pl-2">500 CHF</td>
+            </tr>
+          </table>
+        </div>
+      </main>
       <Footer />
     </div>
   );
